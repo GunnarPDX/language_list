b52755fe80a449a6b6fc85f210b3b4eb876001b9
Gunnar R <gunnarrosenberg@gmail.com>
2021-04-10 17:22:12 -0700

    update docs and error handling

--- README.md
+++ README.md
@@ -1,21 +1,100 @@
 # LanguageList
 
-**TODO: Add description**
+An Elixir implementation of the language_list ruby gem.
 
 ## Installation
 
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
+This package can be installed
 by adding `language_list` to your list of dependencies in `mix.exs`:
 
 ```elixir
 def deps do
   [
-    {:language_list, "~> 0.1.0"}
+    {:language_list, "~> 1.0.0"}
   ]
 end
 ```
 
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at [https://hexdocs.pm/language_list](https://hexdocs.pm/language_list).
+Documentation can be found at [https://hexdocs.pm/language_list](https://hexdocs.pm/language_list).
 
+## Usage:
+
+Requests return a 2-tuple with the standard `:ok` or `:error` status.
+
+#### `all_data`
+
+Returns all language data.
+
+```elixir
+iex> LanguageList.all_data
+{
+  :ok,
+  [
+    %{common: false, iso_639_1: "az", iso_639_3: "aze", name: "Azerbaijani"},
+    %{common: false, iso_639_1: "ba", iso_639_3: "bak", name: "Bashkir"},
+    ...
+  ]
+}
+```
+
+#### `all_common_data`
+
+Returns all common data.
+
+```elixir
+iex> LanguageList.all_common_data
+
+{
+  :ok,
+  [
+    %{common: true, iso_639_1: "af", iso_639_3: "afr", name: "Afrikaans"},
+    %{common: true, iso_639_1: "ar", iso_639_3: "ara", name: "Arabic"},
+    ...
+  ]
+}
+```
+
+#### `languages`
+
+Returns list of all language names.
+
+```elixir
+iex> LanguageList.languages
+
+{:ok, ["Afar", "Abkhazian", "Afrikaans", "Akan", "Amharic", "Arabic", ...]}
+```
+
+#### `common_languages`
+
+Returns list of all common languages.
+
+```elixir
+iex> LanguageList.common_languages
+
+{:ok, ["Afrikaans", "Arabic", "Bengali", "Tibetan", "Bulgarian", ...]}
+```
+
+#### `find(query, key)`
+
+Allows for query of language data by attribute.
+
+Permitted keys: `:name` , `:iso_639_3` , `:iso_639_1`
+
+```elixir
+iex> LanguageList.find("Icelandic", :name)
+
+{:ok, %{common: true, iso_639_1: "is", iso_639_3: "isl", name: "Icelandic"}}
+
+iex> LanguageList.find("pt", :iso_639_1) 
+
+{:ok, %{common: true, iso_639_1: "pt", iso_639_3: "por", name: "Portuguese"}}
+
+iex> LanguageList.find("por", :iso_639_3)
+
+{:ok, %{common: true, iso_639_1: "pt", iso_639_3: "por", name: "Portuguese"}}
+
+iex> LanguageList.find("non-existent-language", :name)
+
+{:error, "No matches found"}
+
+```
--- lib/language_list.ex
+++ lib/language_list.ex
@@ -1,32 +1,23 @@
 defmodule LanguageList do
   @moduledoc """
-  Documentation for `LanguageList`.
+  An Elixir implementation of the language_list ruby gem.
   """
 
   @doc """
-  All data.
+  Returns all language data.
 
   ## Examples
 
-      iex> LanguageList.all_data()
-      [
-        %{
-          "common" => false,
-          "iso_639_1" => "az",
-          "iso_639_3" => "aze",
-          "name" => "Azerbaijani"
-        },
-        %{
-          "common" => false,
-          "iso_639_1" => "ba",
-          "iso_639_3" => "bak",
-          "name" => "Bashkir"
-        },
-        ...
-      ]
-
+      iex> LanguageList.all_data
+      {
+        :ok,
+        [
+          %{common: false, iso_639_1: "az", iso_639_3: "aze", name: "Azerbaijani"},
+          %{common: false, iso_639_1: "ba", iso_639_3: "bak", name: "Bashkir"},
+          ...
+        ]
+      }
   """
-
   def all_data do
     file_path = Application.app_dir(:language_list, "priv/languages.json")
 
@@ -39,36 +30,88 @@ defmodule LanguageList do
     end
   end
 
+  @doc """
+  Returns all common data.
+
+  ## Examples
+
+      iex> LanguageList.all_common_data
+      {
+        :ok,
+        [
+          %{common: true, iso_639_1: "af", iso_639_3: "afr", name: "Afrikaans"},
+          %{common: true, iso_639_1: "ar", iso_639_3: "ara", name: "Arabic"},
+          ...
+        ]
+      }
+  """
   def all_common_data do
     case all_data do
-      {:ok, results} -> Enum.filter(results, fn l -> l.common end)
+      {:ok, results} -> {:ok, Enum.filter(results, fn l -> l.common end)}
       err -> err
     end
   end
 
+  @doc"""
+  Returns list of all language names.
+
+  ## Examples
+
+      iex> LanguageList.languages
+      {:ok, ["Afar", "Abkhazian", "Afrikaans", "Akan", "Amharic", "Arabic", ...]}
+  """
   def languages do
     case all_data do
-      {:ok, results} ->
-        Enum.map(results, fn l -> l.name end)
+      {:ok, results} -> {:ok, Enum.map(results, fn l -> l.name end)}
       err -> err
     end
   end
 
+  @doc """
+  Returns list of all common languages.
+
+  ## Examples
+
+      iex> LanguageList.common_languages
+      {:ok, ["Afrikaans", "Arabic", "Bengali", "Tibetan", "Bulgarian", ...]}
+  """
   def common_languages do
     case all_data do
       {:ok, results} ->
-        results
-        |> Enum.filter(fn l -> l.common end)
-        |> Enum.map(fn l -> l.name end)
+        results =
+          results
+          |> Enum.filter(fn l -> l.common end)
+          |> Enum.map(fn l -> l.name end)
+
+        {:ok, results}
 
       err ->
         err
     end
   end
 
+  @doc """
+  Allows for query of language data by attribute.
+
+  Permitted keys: `:name` , `:iso_639_3` , `:iso_639_1`
+
+  ## Examples
+
+      iex> LanguageList.find("Icelandic", :name)
+      {:ok, %{common: true, iso_639_1: "is", iso_639_3: "isl", name: "Icelandic"}}
+
+      iex> LanguageList.find("pt", :iso_639_1)
+      {:ok, %{common: true, iso_639_1: "pt", iso_639_3: "por", name: "Portuguese"}}
+
+      iex> LanguageList.find("por", :iso_639_3)
+      {:ok, %{common: true, iso_639_1: "pt", iso_639_3: "por", name: "Portuguese"}}
+
+      iex> LanguageList.find("non-existent-language", :name)
+      {:error, "No matches found"}
+  """
   def find(query, :name), do: search(query, :name)
   def find(query, :iso_639_1), do: search(query, :iso_639_1)
-  def find(query, :iso_639_1), do: search(query, :iso_639_3)
+  def find(query, :iso_639_3), do: search(query, :iso_639_3)
   def find(_, _), do: {:error, "Invalid key!"}
 
   defp search(query, key) do
